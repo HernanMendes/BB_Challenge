608ec027905abe184b28aae45f0f706ef80b9af5
HernanMendes <hmendesgouveia@gmail.com>
2022-02-19 17:14:12 -0300

    feat: se obtiene la metadata de cada contenido: título, año, sinopsis, enlace web, géneros

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from urllib import response
 import requests
 import json
+from math import ceil
 
 LIMITE = 50
 PRIMER_PAG = 1
@@ -17,10 +18,22 @@ def guardarPelicula (response_json, contenidos):
             movie["year"] = response_json["data"]["movies"][i]["year"]
             movie["synopsis"] = response_json["data"]["movies"][i]["synopsis"]
             movie["url"] = response_json["data"]["movies"][i]["url"]
-            movie["genres"] = response_json["data"]["movies"][i]["genres"]
+            try:
+                movie["genres"] = response_json["data"]["movies"][i]["genres"]
+            except KeyError:
+                movie["genres"] = []
             contenidos.append(movie)
 
     return contenidos
+
+# Con el movie_count y el limite de paginas por consulta calculamos cuantas paginas deberemos consultar en total
+
+def calcularPaginas (response_json):
+    movie_count = response_json["data"]["movie_count"]
+    limit = response_json["data"]["limit"]
+    total_pages = ceil(movie_count/limit)
+
+    return total_pages
 
 if __name__ == '__main__':
     contenidos = []
@@ -32,10 +45,20 @@ if __name__ == '__main__':
     if response.status_code == OK:
         response_json = json.loads(response.text)       #Diccionario
         #print(response_json)
-        
         ##print(type(json.dumps(response_json, indent=4)))
         ##print(response_json["data"]["movies"][1]["id"])
 
-        contenidos = guardarPelicula(response_json,contenidos)
+        total_pages = calcularPaginas(response_json)
+        print("Total de paginas: "+str(total_pages))
 
-        print(contenidos)
+        for page in range (total_pages):
+            args["page"] = page
+            response = requests.get(url_list, params=args)
+
+            if response.status_code == OK:
+                response_json = json.loads(response.text)
+                contenidos = guardarPelicula(response_json,contenidos)
+
+            print("Pagina "+str(page)+" leida.")
+
+        print(json.dumps(contenidos, indent=4))
